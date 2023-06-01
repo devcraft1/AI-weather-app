aec895b319588db23ef5f7217b7115cdcf057947
devcraft1 <ukonudennis64@gmail.com>
2023-06-01 03:01:03 -1100

    --

---
+++ weather-app/src/WeatherDisplay.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const WeatherDisplay = () => {
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get('your_weather_api_endpoint');
+            setWeatherData(response.data);
+        };
+
+        fetchData();
+    }, []);
+
+    // Render weather information
+    return (
+        <div>
+            {/* Render weather data */}
+        </div>
+    );
+};
+
+export default WeatherDisplay;
